f076568624eba9e64b95b0c2514ca301d335fa96
Yu <piyarat_th@cmu.ac.th>
2026-02-13 10:33:06 +0700

    lab18 completed

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,44 +1,20 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 struct Rect{
-	double x,y,w,h;
+    double x, y, w, h;
 };
 
-double overlap(Rect A,Rect B){
-	double s_width,e_width,s_height,e_height,width,height;
-	if(A.x>B.x){
-		s_width = A.x;
-	}
-	else{
-		s_width = B.x;
-	}
-	if(A.x+A.w<B.x+B.w){
-		e_width = A.x+A.w;
-	}
-	else{
-		e_width = B.x+B.w;
-	}
-	width = e_width-s_width;
+double overlap(Rect A, Rect B){
+    double x_overlap = min(A.x + A.w, B.x + B.w) - max(A.x, B.x);
 
-	if(A.y>B.y){
-		s_height = A.y;
-	}
-	else{
-		s_height = B.y;
-	}
-	if(A.y+A.h<B.y+B.h){
-		e_height = A.y+A.h;
-	}
-	else{
-		e_height = B.y+B.h;
-	}
-	height = e_height-s_height;
+    double y_overlap = min(A.y, B.y) - max(A.y - A.h, B.y - B.h);
 
-	if(height <= 0 || width <= 0){
-		return 0;
-	}
-	else{
-		return height*width;
-	}
+    if(x_overlap > 0 && y_overlap > 0){
+        return x_overlap * y_overlap;
+    }
+    else{
+        return 0;
+    }
 }
